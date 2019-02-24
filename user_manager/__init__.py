717e34a56530af015462a3ba6d27e3c2c7a54330
Mark Drummond <dmark@users.noreply.github.com>
2019-02-23 20:42:40 -0500

    Refactors module to package, merges Auth0 quick start code.

---
+++ user_manager/__init__.py
@@ -0,0 +1,186 @@
+from functools import wraps
+import json
+from os import environ as env
+from werkzeug.exceptions import HTTPException
+
+from dotenv import load_dotenv, find_dotenv
+
+from flask import Flask
+from flask import jsonify
+from flask import redirect
+from flask import request
+from flask import render_template
+from flask import session
+from flask import url_for
+
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+from authlib.flask.client import OAuth
+from six.moves.urllib.parse import urlencode
+
+import constants
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
+AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
+AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
+AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
+AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+if AUTH0_AUDIENCE is '':
+    AUTH0_AUDIENCE = AUTH0_BASE_URL + '/userinfo'
+
+
+class NewUserForm(FlaskForm):
+    given_name = StringField('Given Name', validators=[DataRequired()])
+    family_name = StringField('Family Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Add')
+
+
+class GetUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Get')
+
+
+def create_app():
+    app = Flask(__name__, static_url_path='/public', static_folder='./public')
+    Bootstrap(app)
+    return app
+
+
+app = create_app()
+app.config['SECRET_KEY'] = '03e930e7090ea7f0096881067415f9f7'
+app.secret_key = constants.SECRET_KEY
+app.debug = True
+
+oauth = OAuth(app)
+auth0 = oauth.register(
+    'auth0',
+    client_id=AUTH0_CLIENT_ID,
+    client_secret=AUTH0_CLIENT_SECRET,
+    api_base_url=AUTH0_BASE_URL,
+    access_token_url=AUTH0_BASE_URL + '/oauth/token',
+    authorize_url=AUTH0_BASE_URL + '/authorize',
+    client_kwargs={
+        'scope': 'openid profile',
+    },
+)
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if constants.PROFILE_KEY not in session:
+            return redirect('/login')
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+@app.route('/')
+def home():
+    print(app.url_map)
+    return render_template('index.html')
+
+
+@app.route('/user-agent')
+def user_agent():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+
+@app.route('/callback')
+def callback_handling():
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    session[constants.JWT_PAYLOAD] = userinfo
+    session[constants.PROFILE_KEY] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/dashboard')
+
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL,
+                                    audience=AUTH0_AUDIENCE)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    params = {'returnTo': url_for('index', _external=True),
+              'client_id': AUTH0_CLIENT_ID}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+
+@app.route('/dashboard')
+@requires_auth
+def dashboard():
+    return render_template('dashboard.html',
+                           userinfo=session[constants.PROFILE_KEY],
+                           userinfo_pretty=json.dumps(session[constants.JWT_PAYLOAD],
+                                                      indent=4))
+
+
+@app.route('/user')
+@requires_auth
+def user(name):
+    return render_template('user.html')
+
+
+@app.route('/user_id/<user_id>')
+def profile(user_id):
+    """ Use this to display user user_id's profile. This can be called as a
+    function like so: url_for('profile', user_id=userinfo['sub'])
+    """
+    return '<h1>Profile for %s</h1>' % user_id
+
+
+@app.route('/user/add', methods=['GET', 'POST'])
+@requires_auth
+def user_add():
+    form = NewUserForm()
+    return render_template('adduser.html', form=form)
+
+
+@app.route('/user/get', methods=['GET', 'POST'])
+@requires_auth
+def user_get():
+    form = GetUserForm()
+    return render_template('getuser.html', form=form)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(Exception)
+def handle_auth_error(ex):
+    response = jsonify(message=str(ex))
+    response.status_code = (ex.code if isinstance(ex, HTTPException) else 500)
+    return response
+
+
+if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=env.get('PORT', 5000), debug=True)
+    app.run(debug=True)
